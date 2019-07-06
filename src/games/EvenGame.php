1cb0240ec309add978118e30bc500e8ce5f17540
Oleg Gudzii <oleg198679@gmail.com>
2019-07-06 23:36:26 +0300

    Fix linter errors

--- src/games/EvenGame.php
+++ src/games/EvenGame.php
@@ -10,8 +10,7 @@ function run()
 {
     $message = "Answer \"yes\" if number is even otherwise answer \"no\".\n";
 
-    $getGameData = function ()
-    {
+    $getGameData = function () {
         $gameData = array();
         $gameData[] = rand(0, 100);
         $gameData[] = $gameData[0] % 2 === 0 ? 'yes' : 'no';
